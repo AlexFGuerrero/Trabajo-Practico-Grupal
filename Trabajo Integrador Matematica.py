ea03823cc26d75d248c3281c854cf8fd7c3ac4c7
GasparInacio <gaspar.inac23@gmail.com>
2025-04-16 16:56:39 -0300

    modificaciones

--- Trabajo Integrador Matematica.py
+++ Trabajo Integrador Matematica.py
@@ -1,24 +1,38 @@
-a = -1
-b = -1
-tipo = -1
+a = 0
+b = 0
+tipo = 0
 
-while not(tipo == 0 or tipo == 1 or tipo == 2):
-    tipo = int(input("elija el tipo de operacion logica a realizar: \n 'OR' = 0   'AND' = 1  'NOT' = 2"))
+while True:
+    # Solicitar datos de entrada
+    tipo = int(
+        input(
+            "Elija el tipo de operacion logica a realizar:\n \t1. OR\n   \t2. AND\n  \t3. NOT\n \t4. Salir \n\tElección: "
+        )
+    )
 
-while not(a == 0 or a == 1):
-    a = int(input("Ingrese el valor 'A'(0/1): "))
-
-if tipo != 2:
-    while not(b == 0 or b == 1):
+    # comprobar que todos los datos estén ingresados
+    if tipo == 4:
+        print("Saliendo...")
+        break
+    elif tipo not in [1, 2, 3]:
+        print("Ingrese una opción correcta")
+        continue
+    else:
+        a = int(input("Ingrese el valor 'A'(0/1): "))
         b = int(input("Ingrese el valor 'B'(0/1): "))
 
+    if a not in [0, 1] or b not in [0, 1]:
+        print("A o B deben ser 0 o 1")
+        continue
 
-if tipo == 0:
-    resultado = a or b
-    print(f"A or B = {int(resultado)}")
-elif tipo == 1:
-    resultado = a and b
-    print(f"A and B = {int(resultado)}")
-else:
-    resultado = not(a)
-    print(f"not(A) = {int(resultado)}")
+    # Realizar las tablas
+    if tipo == 1:
+        resultado = a or b
+        print(f"A or B = {int(resultado)}")
+    elif tipo == 2:
+        resultado = a and b
+        print(f"A and B = {int(resultado)}")
+    elif tipo == 3:
+        not_a = not (a)
+        not_b = not (b)
+        print(f"not A = {not_a}\nnot B = {not_b}")
