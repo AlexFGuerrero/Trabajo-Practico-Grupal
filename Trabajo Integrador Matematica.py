2bbea47d8f160b34e5fbb901ad350882840f8471
matias92g <matias92meg@gmail.com>
2025-04-20 16:41:00 -0300

    Agregé estructura repetitiva para que el programa no se reinicie en su totalidad cuando el valor ingresado por el usuario sea != 0 or 1

--- Trabajo Integrador Matematica.py
+++ Trabajo Integrador Matematica.py
@@ -17,22 +17,35 @@ while True:
     elif tipo not in [1, 2, 3]:
         print("Ingrese una opción correcta")
         continue
-    else:
+    elif tipo == 1:
         a = int(input("Ingrese el valor 'A'(0/1): "))
         b = int(input("Ingrese el valor 'B'(0/1): "))
-
-    if a not in [0, 1] or b not in [0, 1]:
-        print("A o B deben ser 0 o 1")
-        continue
+        while a not in [0, 1] or b not in [0, 1]:
+            print("A o B deben ser 0 o 1")
+            a = int(input("Ingrese nuevamente un valor para 'A'(0/1): "))
+            b = int(input("Ingrese nuevamente un valor para 'B'(0/1): "))
+        
+    elif tipo == 2:
+        a = int(input("Ingrese el valor 'A'(0/1): "))
+        b = int(input("Ingrese el valor 'B'(0/1): "))
+        while a not in [0, 1] or b not in [0, 1]:
+            print("A o B deben ser 0 o 1")
+            a = int(input("Ingrese nuevamente un valor para 'A'(0/1): "))
+            b = int(input("Ingrese nuevamente un valor para 'B'(0/1): "))
+        
+    elif tipo == 3:
+        a = int(input("Ingrese un valor (0/1): "))
+        while a not in [0, 1] or b not in [0, 1]:
+            print("A o B deben ser 0 o 1")
+            a = int(input("Ingrese nuevamente un valor para 'A'(0/1): "))
 
     # Realizar las tablas
     if tipo == 1:
         resultado = a or b
-        print(f"A or B = {int(resultado)}")
+        print(f"A or B = {resultado}")
     elif tipo == 2:
         resultado = a and b
-        print(f"A and B = {int(resultado)}")
+        print(f"A and B = {resultado}")
     elif tipo == 3:
         not_a = not (a)
-        not_b = not (b)
-        print(f"not A = {not_a}\nnot B = {not_b}")
+        print(f"not A = {not_a}")
