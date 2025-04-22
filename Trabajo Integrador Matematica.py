563bddff0c987d9c13a7c4877d7e75965af12155
AlexFGuerrero <ale.guerrero@outlook.com>
2025-04-22 18:40:06 -0300

    Me Había equivocado en un comentario

--- Trabajo Integrador Matematica.py
+++ Trabajo Integrador Matematica.py
@@ -9,8 +9,7 @@ while True:
             "Elija el tipo de operacion logica a realizar:\n \t1. OR\n   \t2. AND\n  \t3. NOT\n \t4. Salir \n\tElección: "
         )
     )
-    # # Interfaz de entrada (menú)
-    if tipo == 4:
+ # Comprobando datos ingresados    if tipo == 4:
         print("Saliendo...")
         break
     # Nos aseguramos que los datos de menú ingresados sean correctos
