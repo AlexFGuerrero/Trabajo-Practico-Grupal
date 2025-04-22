069131d69ea33b6130e5855fb64abc5f40c29b75
AlexFGuerrero <ale.guerrero@outlook.com>
2025-04-22 18:30:39 -0300

    Codigo comentado

--- Trabajo Integrador Matematica.py
+++ Trabajo Integrador Matematica.py
@@ -3,22 +3,25 @@ b = 0
 tipo = 0
 
 while True:
-    # Solicitar datos de entrada
+    # Interfaz de entrada (menú)
     tipo = int(
         input(
             "Elija el tipo de operacion logica a realizar:\n \t1. OR\n   \t2. AND\n  \t3. NOT\n \t4. Salir \n\tElección: "
         )
     )
-    # comprobar que todos los datos estén ingresados
+    # # Interfaz de entrada (menú)
     if tipo == 4:
         print("Saliendo...")
         break
+    # Nos aseguramos que los datos de menú ingresados sean correctos
     elif tipo not in [1, 2, 3]:
         print("Ingrese una opción correcta")
         continue
+    # Según la opción de menú seleccionada pedimos valores de entrada
     elif tipo == 1:
         a = int(input("Ingrese el valor 'A'(0/1): "))
         b = int(input("Ingrese el valor 'B'(0/1): "))
+        # Comprobamos que los datos sean valores permitidos
         while a not in [0, 1] or b not in [0, 1]:
             print("A o B deben ser 0 o 1")
             a = int(input("Ingrese nuevamente un valor para 'A'(0/1): "))
@@ -27,6 +30,7 @@ while True:
     elif tipo == 2:
         a = int(input("Ingrese el valor 'A'(0/1): "))
         b = int(input("Ingrese el valor 'B'(0/1): "))
+        # Comprobamos que los datos sean valores permitidos
         while a not in [0, 1] or b not in [0, 1]:
             print("A o B deben ser 0 o 1")
             a = int(input("Ingrese nuevamente un valor para 'A'(0/1): "))
@@ -34,6 +38,7 @@ while True:
         
     elif tipo == 3:
         a = int(input("Ingrese un valor (0/1): "))
+        # Comprobamos que los datos sean valores permitidos
         while a not in [0, 1] or b not in [0, 1]:
             print("A o B deben ser 0 o 1")
             a = int(input("Ingrese nuevamente un valor para 'A'(0/1): "))
